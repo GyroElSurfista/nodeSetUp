2aa6091365b7b5fbbe82018818e8f9a77c22527b
Jairo Maida <gyroelsurfista@gmail.com>
2024-06-02 21:27:48 -0400

    feat: middleware log

--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const logginMiddleware = require('../middleware/loggin.middleware')
 const healthController = require('../controllers/health.controller')
 
 
